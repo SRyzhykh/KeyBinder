4a83f5e2de429983818481d6682d869b2dc746e4
Serhii Ryzhykh <serhiiryzhykh@MacBook-Air-Serhii.local>
2023-02-02 09:59:45 +0200

    added navigation

--- src/screens/WelcomeScreen/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen/WelcomeScreen.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import {View, Button} from 'react-native';
 import {fetchData} from '../../helpers';
 import {styles} from './WelcomeScreen.styles';
+// import {useNavigation} from '@react-navigation/native';
 
 export const WelcomeScreen = () => {
+  // const navigation = useNavigation();
   const onPress = () => {
     fetchData();
+    // navigation.navigate();
   };
 
   return (
